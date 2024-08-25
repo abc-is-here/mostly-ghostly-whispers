c2dc946101f3df1075492231c0be8489bb946c01
abc <abhaygupta.hsj@gmail.com>
2024-08-25 16:56:00 +0530

    Flashlight somewhat works!

--- scripts/flashlight.gd
+++ scripts/flashlight.gd
@@ -1,6 +1,7 @@
 extends SpotLight3D
 
+var picked_up = false
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("flashlight"):
+	if Input.is_action_just_pressed("flashlight") && picked_up == true:
 		visible = !visible
